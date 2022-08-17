396f6268fb92a2cbd7da269e9a98d2574276936e
Maidul Hasan <maidul.hasan@gmail.com>
2022-08-18 02:44:00 +1000

    Initial Commit

---
+++ src/main/java/com/hashnet/App.java
@@ -0,0 +1,13 @@
+package com.hashnet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
